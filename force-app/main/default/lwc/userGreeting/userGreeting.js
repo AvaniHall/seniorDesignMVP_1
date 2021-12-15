a240a66e2d128e55bd26a0240ac5ce87c05b194d
Avani Hall <avanichall@gmail.com>
2021-12-14 22:29:35 -0500

    progress at due date for mvp

---
+++ force-app/main/default/lwc/userGreeting/userGreeting.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+
+export default class UserGreeting extends LightningElement {
+    user = "Avani Hall";
+  classStatus = "Senior (2022)";
+  numOfCredits = "254/272";
+}
