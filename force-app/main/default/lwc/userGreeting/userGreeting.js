6e0402cb3e0cbce8d8526985079a6f62ecb900c6
AvaniHall <achall@captechu.edu>
2022-04-14 13:54:56 -0400

    final changes for final report

--- force-app/main/default/lwc/userGreeting/userGreeting.js
+++ force-app/main/default/lwc/userGreeting/userGreeting.js
@@ -1,10 +1,8 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import showCourses from '@salesforce/apex/classPortal.showCourses';
 
 export default class UserGreeting extends LightningElement {
-  @api studentId;
-  @wire(showCourses, {studentId: '$studentId'})
-  courses;
+  //@wire(showCourses) courses;
   user = ["Avani Hall"];
   classStatus = "Senior (2022)";
   numOfCredits = "254/272";
